9d31dbd31c7c725331288d27ef8ff8fa77800efa
JasonArrechea <jfernandoibarguen@estudiante.uniajc.edu.co>
2025-11-07 11:20:55 -0500

    feat(auth): implementación de página y formulario de login

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import LoginForm from '../../components/auth/LoginForm'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Login() {
+    return (
+        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '80vh' }}>
+            <div style={{ maxWidth: 420, width: '100%' }}>
+                <LoginForm />
+
+                <div className="text-center mt-3">
+                    <Link to="/">Ir a Home</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
