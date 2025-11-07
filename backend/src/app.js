dbf2fba22c37d997e470ab410a4d93d9c9fab39f
Camilo Gómez <gomezcamilo1503@gmail.com>
2025-11-07 11:51:41 -0500

    feat(user): mejoras en gestión de usuarios — subida única de foto, eliminación solo admin, perfil y actualización funcionales

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js";
+
 
 dotenv.config(); 
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
 
+// 🌎 Middlewares
 app.use(cors());
 app.use(express.json());
 
+// 🛠️ Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+// 📁 Ruta para servir archivos estáticos (imágenes de perfil)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
   res.send("AgroMarket Backend funcionando correctamente");
--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 export const generateToken = (user) => {
   return jwt.sign(
     {
-    id: user.id_usuario,
-    nombre: user.nombre,
-    correo: user.correo,
-    rol: user.rol,
+      id_usuario: user.id_usuario,
+      nombre: user.nombre,
+      correo: user.correo,
+      rol: user.rol, 
     },
     process.env.JWT_SECRET,
-    { expiresIn: "7d"}
+    { expiresIn: "7d" }
   );
 };
